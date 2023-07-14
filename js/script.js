2f8a78c2cc30fc55f35bc24192bb10c64adfd3e7
Zander Sacay <zandersacay@gmail.com>
2023-07-14 17:06:24 +0800

    Altered getPlayerChoice, added playRound function
    getPlayerChoice now returns the selection instead of storing in a separate value,
    previously caused issues when calling the playRound function with the
    getPlayerChoice and getComputerChoice functions are args.

--- js/script.js
+++ js/script.js
@@ -4,15 +4,33 @@ function getComputerChoice() {
     return choices[index];
 }
 
-let playerSelection;
-
 function getPlayerChoice() {
     let temp = prompt("Enter your choice (rock/ paper/ scissors): ").toLowerCase();
     if (temp != "rock" && temp != "paper" && temp != "scissors") {
         alert("Invalid input. Please choose between (rock/ paper/ scissors).");
         getPlayerChoice();
     } else {
-        playerSelection = temp;
+        return temp;
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Draw! You chose the same hand.";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose. Paper beats rock.";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose. Scissors beat paper.";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose. Rock beats scissors.";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats rock!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beat paper!";
+    } else {
+        return "Something went wrong";
     }
 }
 
