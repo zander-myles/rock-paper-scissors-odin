8dbba69bb21f0a5bd25efdcd95ece56e717db059
Zander Sacay <zandersacay@gmail.com>
2023-07-14 15:05:23 +0800

    Added getPlayerChoice function

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ function getComputerChoice() {
     let index = Math.floor(Math.random() * 3);
     return choices[index];
 }
+
+let playerSelection;
+
+function getPlayerChoice() {
+    let temp = prompt("Enter your choice (rock/ paper/ scissors): ").toLowerCase();
+    if (temp != "rock" && temp != "paper" && temp != "scissors") {
+        alert("Invalid input. Please choose between (rock/ paper/ scissors).");
+        getPlayerChoice();
+    } else {
+        playerSelection = temp;
+    }
+}
+
